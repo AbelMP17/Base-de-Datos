ae56b713072a4edd13bfc2b27edda6cefa9f522b
amp18 <95914401+AbelMP17@users.noreply.github.com>
2023-03-07 16:37:22 +0100

    prueba git

--- Unidad 4/Ejercicio36.sql
+++ Unidad 4/Ejercicio36.sql
@@ -1,5 +1,7 @@
 /*36. Realizar una consulta (utilizando subconsultas) que obtenga los distintos ponentes que han usado la sala
 “Afrodita” para dar una conferencia*/
+-------------------------------------------------------------------------------------------------------------------
+
 SELECT ponente.*
 FROM ponente JOIN participar ON(ponente.codigo = participar.codPonente)
                 JOIN conferencia ON(participar.refConferencia = conferencia.referencia)
