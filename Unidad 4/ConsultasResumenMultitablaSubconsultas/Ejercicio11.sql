de5f60348a0550f9aa8906eb7f57c015a0b23ea7
AbelMP17 <abelmp890@gmail.com>
2023-03-13 10:50:01 +0100

    hol

--- Unidad 4/ConsultasResumenMultitablaSubconsultas/Ejercicio11.sql
+++ Unidad 4/ConsultasResumenMultitablaSubconsultas/Ejercicio11.sql
@@ -1,17 +1,4 @@
 /*11. Obtener el nombre de los productos con mayor stock que compra un cliente
 cuyo representante de ventas trabaje en la oficina de la ciudad de Madrid.*/
 
--- Esta mal
-SELECT producto.nombre
-FROM producto
-JOIN detalle_pedido USING(codigo_producto)
-JOIN pedido USING(codigo_pedido)
-JOIN cliente USING(id_cliente)
-JOIN empleado ON(cliente.id_empleado_rep_ventas = empleado.id_empleado)
-JOIN oficina USING(codigo_oficina)
-WHERE oficina.ciudad = 'Madrid'
-    AND cliente.id_cliente IN(SELECT id_cliente
-                                FROM pedido
-                                GROUP BY id_cliente
-                                ORDER BY SUM(cantidad) DESC)
-ORDER BY detalle_pedido.cantidad DESC;
+
