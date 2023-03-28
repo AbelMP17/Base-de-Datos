26550b7027fbfb400a60091c343679bb874bbe79
AbelMP17 <abelmp890@gmail.com>
2023-03-28 20:21:08 +0200

    EjerciciosU5 Hecho

--- Unidad 5/EjerciciosU5/EjerciciosU5.sql
+++ Unidad 5/EjerciciosU5/EjerciciosU5.sql
@@ -87,3 +87,77 @@ UPDATE producto SET precio = precio * 1.05 WHERE id_fabricante IN(SELECT id
                                                                     WHERE nombre LIKE "_e%");
 
 /*====================================================================================*/
+
+/*14. Añadir un “*” al final del nombre del fabricante para aquellos en los que el precio medio de sus productos sea
+superior a 200.*/
+UPDATE fabricante SET nombre = CONCAT(nombre, '*') WHERE id IN ( SELECT id_fabricante 
+                                                                FROM producto GROUP BY id_fabricante 
+                                                                HAVING AVG(precio) > 200); 
+
+/*15. Eliminar todos los fabricantes que no tienen productos asignados.*/
+DELETE FROM fabricante WHERE nombre NOT IN (SELECT nombre_fabricante
+                                            FROM fabricante_productos);
+
+/*====================================================================================*/
+
+/*16. Insertar un nuevo socio con los siguientes datos:*/
+INSERT INTO socio VALUES (1007, "Francisco", "Sanchez", "Avda de las Palmeras, 2", "Armilla", "1970/02/02");
+
+/*====================================================================================*/
+
+/*17. Registrar el primer alquiler del socio 1007 que se lleva la copia 105 (correspondiente a la película "Lo
+imposible") en la fecha actual (en la que se está haciendo la tarea). */
+INSERT INTO alquiler (copia_pel, socio, fec_alquila) VALUES (105, 1007, CURDATE());
+
+/*====================================================================================*/
+
+/*18. Modificar el estado de la copia de la película con id 101 a "ESTROPEADA" incluyendo como observación
+"Rayado".*/
+UPDATE copia_pelicula SET estado = "ESTROPEADA" , observacion = "Rayado"
+WHERE id_copia = 101;
+
+/*====================================================================================*/
+
+/*19. Eliminar la película cuyo título es "El Orfanato".*/
+DELETE FROM pelicula WHERE titulo = "El Orfanato";
+
+/*====================================================================================*/
+
+/*20. Insertar una nueva película (inventando los datos) y después dar de alta dos copias para dicha película con el
+estado "FUNCIONA".*/
+INSERT INTO pelicula VALUES ("EN", "A Todo Gas", 109, 2010, "Accion");
+INSERT INTO copia_pelicula  VALUES (120, "FUNCIONA", null, "EN"), (122, "FUNCIONA", null, "EN");
+
+/*====================================================================================*/
+
+
+/*21. Actualizar todas las películas que tengan como género "Animación" y reemplazarlo por "Dibujos".*/
+UPDATE pelicula SET genero = "Dibujos"
+WHERE genero = "Animacion";
+
+/*====================================================================================*/
+
+/*22. Eliminar aquellos socios cuya última película alquilada sea anterior al 1 de diciembre de 2014.*/
+DELETE FROM socio
+WHERE num_socio IN (SELECT socio
+                    FROM alquiler
+                    WHERE fec_devolucion < "2014-12-01");
+
+/*====================================================================================*/
+
+/*23. Incrementar en 20 céntimos el precio del alquiler a todas las películas que tengan más de dos copias.*/
+UPDATE pelicula SET precio_alquiler = precio_alquiler + 0.20 WHERE codigo IN (SELECT pelicula
+                                                                                FROM copia_pelicula
+                                                                                GROUP BY pelicula
+                                                                                HAVING COUNT(*) > 2);
+
+/*====================================================================================*/
+
+/*24. Eliminar todas las copias de las películas que contengan la palabra "FROZEN" y que su estado sea
+"ESTROPEADA".*/
+DELETE FROM copia_pelicula
+WHERE estado LIKE "ESTROPEADA" AND pelicula IN (SELECT codigo
+                                                FROM pelicula
+                                                WHERE titulo LIKE "FROZEN");
+
+/*====================================================================================*/
